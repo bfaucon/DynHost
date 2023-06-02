1e7c1e9eb1d0692f5b5196116d143dd406f35a72
Bruno Faucon <bruno.faucon@one2care.be>
2023-06-02 17:44:00 +0200

    Update Remarks

--- CheckWan4.sh
+++ CheckWan4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #=========================================================================
-#              CheckWan.py
+#              CheckWan4.py
 #-------------------------------------------------------------------------
 # by Bfaucon - 2022, April
 # version 0.1 2022-04-20
@@ -11,6 +11,7 @@
 #-------------------------------------------------------------------------
 # Checking internet connection.
 # If issue, restart wlan0
+# The good version for RPI4
 #=========================================================================
 
 # keep LAN alive
@@ -18,6 +19,7 @@ echo "................................................"
 date "+%Y.%m.%d %H:%M:%S"
 echo " "
 ping -c2 google.com 
+
  
 if [ $? != 0 ] 
 then 
@@ -34,7 +36,7 @@ then
    echo "Wait 30 Sec"
    sleep 30s
    echo "send Email"
-   python ./SendMail.py rpi@one2care.be bruno.faucon@one2care.be 'Alerte Raspberry RPI4-PROD' 'Problème de connexion internet sur le nouveau Raspberry. Je redémarre wlan0'
+   python SendMail.py rpi@one2care.be bruno.faucon@one2care.be 'Alerte Raspberry' 'Problème de connexion internet sur le nouveau Raspberry. Je redémarre wlan0'
 fi
 echo "................................................"
 echo " "
